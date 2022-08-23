e894f391cef38237bbbb73244d04cc97151f6b72
ThemDankMeme <duvalvdv@gmail.com>
2022-08-23 18:19:50 +0200

    API functionality extended to get pagination need to implement page count on javascript to auto increment and decrement page count when sales rows are returned in JSON format

--- API.php
+++ API.php
@@ -23,7 +23,7 @@ class API{
 
             }
         }
-        catch (Exception $err){
+        catch (PDOException $err){
             echo $err->getMessage();
         }
     }
@@ -31,20 +31,113 @@ class API{
         $json['status']=$status;
         $json['code']= $code;
         $json['timestamp']=time();
-        $json['data']=['Error'=>$data];
+        $json['data']=['message'=>$data];
         return json_encode($json);
     }
     public function createSale(){
 
     }
-    public function readRange(){
+    public function readRange($start, $end, $page):string{
+        $originalStart = $start;
+        $originalEnd = $end;
+        $start = $this->testInput($start);
+        $end = $this->testInput($end);
+        if($this->beforeSubmit($originalStart, $start)&&$this->beforeSubmit($originalEnd, $end)){
+            try {
+                $sql = "SELECT COUNT(*) FROM sales WHERE order_date BETWEEN ? AND ?";
+                $query = $this->conn->prepare($sql);
+                $query->execute([$start, $end]);
+                $total = $query->fetchColumn();
 
+                if($total==0){
+                    return $this->setGeneric('failed', 204, 'No Sales between '.$start.' and '.$end);
+                }else{
+                    $limit = 10;
+                    $pages = ceil($total/$limit);
+                    $offset = ($page-1)*$limit;
+                    $sql = "SELECT order_no, users.name, order_date, amount_ex_vat, total, revised
+                            FROM sales 
+                            INNER JOIN users ON sales.client=users.id
+                            WHERE order_date BETWEEN :start AND :end 
+                            ORDER BY order_date DESC 
+                            LIMIT :limit OFFSET :offset";
+                    $query = $this->conn->prepare($sql);
+                    $query->bindParam(':start', $start, PDO::PARAM_STR);
+                    $query->bindParam(':end', $end, PDO::PARAM_STR);
+                    $query->bindParam(':limit', $limit, PDO::PARAM_INT);
+                    $query->bindParam(':offset', $offset, PDO::PARAM_INT);
+                    $query->execute();
+                    $count = $query->rowCount();
+                    $json = $this->setGeneric('success', 200, 'Sales data between '.($offset+1).' and '.($offset+$count));
+                    $json = json_decode($json);
+                    while ($row = $query->fetch(PDO::FETCH_ASSOC)){
+                        $json['data'][] = [
+                            'order_no'=>$row['order_no'],
+                            'name'=>$row['name'],
+                            'order_date'=>$row['order_date'],
+                            'amount_ex_vat'=> $row['amount_ex_vat'],
+                            'total'=>$row['total'],
+                            'revised'=>$row['revised']
+                        ];
+                    }
+                    echo ("Total : ".$total." Pages: ".$pages);
+                    return json_encode($json);
+                }
+
+            }
+            catch (PDOException $err){
+                return $this->errorMessage($err);
+            }
+        }
+        else
+            return $this->teaPot();
+
+    }
+    private function errorMessage(PDOException $err):string{
+        error_log("Unexpected Error: " . $err->getMessage() . " at " . date_create()->format('Y-m-d H:i:s'));
+        return $this->setGeneric('error', 500, $err->getMessage());
+    }
+    private function teaPot():string{
+        return $this->setGeneric('error', 418, "The server refuses the attempt to brew coffee with a teapot.");
     }
     public function updateSale(){
 
     }
     public function removeSale(){
         
+    }
+    public function findUser($email): string{
+        $original = $email;
+        $email = $this->testInput($email);
+        if($this->beforeSubmit($original, $email)) {
+            try {
+                $sql = "SELECT id FROM users WHERE users.email=?";
+                $query = $this->conn->prepare($sql);
+                $query->execute([$email]);
+                if ($query->rowCount() == 0) {
+                    return $this->setGeneric('failed', 401, 'Invalid User: ' . $email);
+                } else {
+                    return $this->setGeneric('success', 200, 'Valid User: ' . $email);
+                }
+            } catch (PDOException $err) {
+                return $this->errorMessage($err);
+            }
+        }
+        else
+            return $this->teaPot();
+    }
+    private function testInput($value): string
+    {
+        $value = trim($value);
+        $value = stripcslashes($value);
+        return htmlspecialchars($value);
+    }
+
+    private function beforeSubmit($original, $temp): bool
+    {
+        if ($original != $temp)
+            return false;
+        else return true;
     }
 
 
@@ -53,10 +146,12 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     $_POST = (json_decode(file_get_contents("php://input"), true));
     if (isset($_POST['type'])) {
         $instance = new API();
+        $db = DB_NAME;
         if($_POST['type']=='create'){
 
         }
         elseif ($_POST['type']=='read'){
+            $res = $instance->readRange($_POST['start'], $_POST['end']);
 
         }
         elseif ($_POST['type']=='update'){
@@ -65,8 +160,11 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
         elseif ($_POST['type']=='delete'){
 
         }
+        elseif ($_POST['type']=='check'){
+            $res = $instance->findUser($_POST['email']);
+        }
         else{
-            $res = $instance->setGeneric('Failed', 406, 'NO METHOD MATCHING FOUND');
+            $res = $instance->setGeneric('Failed', 501, 'NO METHOD MATCHING FOUND');
         }
         return $res;
 
@@ -74,4 +172,5 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
     }
 }
 $instance = new API();
-$instance->test();
+//$instance->test();
+echo ($instance->readRange('1995-01-01', '2018-12-31', 5));
--- own_js/funtionality.js
+++ own_js/funtionality.js
@@ -1,4 +1,101 @@
-function updatePost(){
-       let url = "http://localhost/phpWeb/API.php";
+function validateEmail(){
+       let valid =  /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+       let mail = document.getElementById("email").value;
+       if(mail.match(valid)){
+              document.getElementById("email").style.background="";
+       }
+       else{
+              document.getElementById("email").style.background = "#F2275D";
+              alert("Email input incorrect");
+       }
+}
 
+
+function callPost(type){
+       let url = "http://localhost/phpWeb/API.php";
+       let email = document.getElementById('email').value;
+       if(checkUser(email)){
+              document.getElementById("email").style.background="";
+              let xhr = new XMLHttpRequest();
+              xhr.open("POST", url);
+              xhr.setRequestHeader("Content-Type", "application/json");
+              xhr.onreadystatechange = function () {
+                     if (xhr.readyState === 4 && xhr.status===200) {
+                            let json = JSON.parse(xhr.responseText);
+                            if(json.status==="success"){
+
+                            }
+                            else if(json.status==="failed"){
+
+                            }
+                     }
+              };
+              let data = setType(type)
+              xhr.send(JSON.stringify(data));
+       }
+       else{
+              document.getElementById("email").style.background = "#F2275D";
+       }
+}
+function checkUser(email){
+       let url = "http://localhost/phpWeb/API.php";
+       let valid;
+       let xhr = new XMLHttpRequest();
+       xhr.open("POST", url);
+       xhr.setRequestHeader("Content-Type", "application/json");
+       xhr.onreadystatechange = function () {
+              if (xhr.readyState === 4 && xhr.status===200) {
+                     let json = JSON.parse(xhr.responseText);
+                     if(json.status==="success"){
+                            valid = true;
+                     }
+                     else if(json.status==='failed'){
+                            alert("No Account Matching: "+ email);
+                            valid = false;
+                     }
+                     else if(json.status==='error'){
+                            alert("Unexpected Error: "+ json.data.message);
+                            valid = false;
+                     }
+              }
+       };
+       let data = {"type": "check", "email": email};
+       xhr.send(JSON.stringify(data));
+       return valid;
+}
+
+function setType(type){
+       let data;
+
+       if(type==='read'){
+              let start = document.getElementById('start').value;
+              let end = document.getElementById('end');
+              let page = document.getElementById('page').value;
+              if(end !==null && end.value===""){
+                     end = new Date();
+                     let dd = end.getDate();
+                     if(dd<10)
+                            dd='0'+dd;
+                     let mm = end.getMonth()+1;
+                     if(mm<10)
+                            mm='0'+mm;
+                     let yy = end.getFullYear();
+                     end = yy+'-'+mm+'-'+dd;
+              }
+              data = {"type": "read", "start": start, "end":end};
+       }
+       else if(type==='update'){
+              let email = document.getElementById('email').value;
+              let order = document.getElementById('order').value;
+              let amount = document.getElementById('amount').value;
+              let total = document.getElementById('total').value;
+              let date = document.get
+              data = {
+                     "type": "update",
+                     "order": order,
+                     "email": email,
+                     "amount": }
+       }
+
+       return data;
 }
